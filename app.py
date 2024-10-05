21e3c47b05ab54c4078fb368e464d2f958edd1b3
Zen L <zenlambertus@gmail.com>
2024-10-05 16:25:02 -0400

    WERE BACK

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import os
+import openai
+from dotenv import load_dotenv
+import ast
+import asyncio
+import datetime
+
+app = Flask(__name__)
+database_url = os.getenv('DATABASE_URL')
+@app.route('/')
+def home():
+    return redirect(url_for('motion'))
+
+@app.route('/MotionBank/home')
+def motion():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(24)
+    app.run()
