37a377e1978a2d30ba915ac08a25bcd6be25f7e5
Zen L <zenlambertus@gmail.com>
2024-10-05 18:00:51 -0400

    load check + comments

--- app.py
+++ app.py
@@ -10,22 +10,25 @@ import ast
 import asyncio
 import datetime
 
+# Set up app and MongoDB connection
 app = Flask(__name__)
+if not load_dotenv():
+    print("---\nDid you make a .env file and add DATABASE_URL to it? -Zen\n---\n")
 database_url = os.getenv('DATABASE_URL')
 client = MongoClient(database_url)
 db = client['Squad']
 
+# Home page redirect
 @app.route('/')
 def home():
     return redirect(url_for('motion'))
 
+# Home page
 @app.route('/MotionBank/home')
 def motion():
     return render_template('index.html')
 
-#config = configparser.ConfigParser()
-#config.read(os.path.abspath(os.path.join(".ini")))
-
+# Connection testing
 @app.route('/test-connection')
 def test_connection():
     try:
@@ -35,6 +38,7 @@ def test_connection():
     except Exception as e:
         return f"Failed to connect to MongoDB Atlas: {e}"
 
+# Run app
 if __name__ == "__main__":
     app.secret_key = os.urandom(24)
     app.run(debug=True)
