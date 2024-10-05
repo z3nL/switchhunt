2b145d38a1adf441c90dbf4df356e84d5f521f10
Zen L <zenlambertus@gmail.com>
2024-10-05 17:44:56 -0400

    huge plays

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
+from flask import Flask, render_template, send_file, request, redirect, url_for, flash, session, jsonify
 # from flask_mysqldb import MySQL
 # import MySQLdb.cursors
 import os
 # import openai
 from dotenv import load_dotenv
 import configparser
+from pymongo import MongoClient
 import ast
 import asyncio
 import datetime
 
 app = Flask(__name__)
 database_url = os.getenv('DATABASE_URL')
+client = MongoClient(database_url)
+db = client['Squad']
 
 @app.route('/')
 def home():
@@ -20,10 +23,18 @@ def home():
 def motion():
     return render_template('index.html')
 
-config = configparser.ConfigParser()
-config.read(os.path.abspath(os.path.join(".ini")))
+#config = configparser.ConfigParser()
+#config.read(os.path.abspath(os.path.join(".ini")))
 
+@app.route('/test-connection')
+def test_connection():
+    try:
+        # Check MongoDB server information
+        server_info = client.server_info()  # Will throw an exception if not connected
+        return send_file('static/images/preston.jpeg', mimetype='image/jpeg')
+    except Exception as e:
+        return f"Failed to connect to MongoDB Atlas: {e}"
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(24)
-    app.run()
+    app.run(debug=True)
